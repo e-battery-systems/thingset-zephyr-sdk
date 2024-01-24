ab3559766d0f4b5d292b8fc1e16d1b56142a9bc8
Roman PASSLER <roman.passler@e-bs.at>
2024-01-24 14:11:18 +0100

    feat: add pub/sub tests for can counter example

--- samples/counter/src/main.c
+++ samples/counter/src/main.c
@@ -5,7 +5,9 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/sys/byteorder.h>
 
+#include "thingset/can.h"
 #include "thingset/sdk.h"
 
 static uint32_t counter;
@@ -27,8 +29,51 @@ THINGSET_ADD_ITEM_UINT32(APP_ID_SAMPLE, APP_ID_SAMPLE_WCOUNTER, "wCounter", &cou
 THINGSET_ADD_ITEM_FLOAT(APP_ID_SAMPLE, APP_ID_SAMPLE_CPI, "cPi", &pi, 2, THINGSET_ANY_R,
                         TS_SUBSET_LIVE | TS_SUBSET_SUMMARY);
 
+static void report_rx_callback(uint16_t data_id, const uint8_t *value, size_t value_len,
+                               uint8_t source_addr)
+{
+    printf("data_id 0x%X src_addr 0x%X length %u raw ", data_id, source_addr, value_len);
+    for (size_t i = 0; i < value_len; i++) {
+        printf("%02x", *(value + i));
+        if (i == 0 && value_len != 1) {
+            printf(" ");
+        }
+    }
+
+    printf(" ");
+
+    struct thingset_data_object *obj = NULL;
+    while ((obj = thingset_iterate_subsets(&ts, TS_SUBSET_LIVE, obj)) != NULL) {
+        if (data_id == obj->id) {
+            break;
+        }
+        obj++;
+    }
+
+    printf("type %u ", obj->type);
+
+    if (obj->type == THINGSET_TYPE_U32) {
+        size_t offset = 0;
+        if (value_len > 1) {
+            offset++;
+        }
+        uint32_t tmp = sys_be32_to_cpu(*(uint32_t *)(value + offset)
+                                       << ((sizeof(uint32_t) - (value_len - offset)) * 8));
+        printf("value %u\n", tmp);
+    }
+    else if (obj->type == THINGSET_TYPE_F32) {
+        uint32_t tmp = sys_be32_to_cpu(*(uint32_t *)(value + 1));
+        printf("value %0.2f\n", *(float *)&tmp);
+    }
+    else {
+        printf("not supported!\n");
+    }
+}
+
 int main(void)
 {
+    thingset_can_set_report_rx_callback(report_rx_callback);
+
     while (true) {
         counter++;
         k_sleep(K_SECONDS(1));
